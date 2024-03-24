d0353ee9d29a6dd1ca04c2d1b9e19cd86e0f7f6f
mudasir-pro <mudasirabbasi26@gmail.com>
2024-03-24 23:58:26 +0500

    basic resolve

--- src/component/Landingpage/NewsAndEvent.js
+++ src/component/Landingpage/NewsAndEvent.js
@@ -30,7 +30,7 @@ const NewsAndEvent = () => {
                     onClick={(e)=>{
                         navigate('/news')
                     }}
-                    className='text-neutral-800 bg-cyan-500 rounded-xl px-8 py-4 text-2xl font-medium font-[Roboto]'>News and Events</button>
+                    className='text-neutral-800 bg-cyan-500 rounded-xl px-8 py-4 text-2xl font-medium font-[Roboto]'>News & Events</button>
                 </div>
                 <div className='px-8'>
                     <img src={Illustration} alt="" />
